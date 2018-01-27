28458db692f167f88acac79ebf74c7911a924fd8
Oliver Steele <steele@osteele.com>
2018-01-27 08:53:09 -0500

    Programmatically generate some tests

--- front/Rating.test.js
+++ front/Rating.test.js
@@ -2,24 +2,32 @@ import { EditRating, Rating } from './Rating.js'
 
 import React from 'react'
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe('Rating', () => {
-    test('makes stars', () => {
-        const component = renderer.create(
-            <Rating rank="2" />
-        );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    })
+
+
+    [0, 1, 5].forEach(rank => {
+        test(`${rank} matches snapshot`, () => {
+
+            const component = renderer.create(
+                <Rating rank={rank} />
+            );
+            let tree = component.toJSON();
+            expect(tree).toMatchSnapshot();
+        });
+    });
 })
 
 
 describe('EditRating', () => {
-    test('makes stars', () => {
-        const component = renderer.create(
-            <EditRating rank="2" />
-        );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+    [0, 1, 5].forEach(rating => {
+        test(`rating ${rating} matches snapshot`, () => {
+            const component = renderer.create(
+                <EditRating rating={rating} />
+            );
+            let tree = component.toJSON();
+            expect(tree).toMatchSnapshot();
+        });
     })
 })
