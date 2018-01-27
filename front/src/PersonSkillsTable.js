a64688ed2012d09a56aac2695bfdbd63a7edffdd
Oliver Steele <steele@osteele.com>
2018-01-27 16:25:30 -0500

    Re-indent PersonSkillsTable.js

--- front/src/PersonSkillsTable.js
+++ front/src/PersonSkillsTable.js
@@ -6,63 +6,63 @@ import { getPersonSkillRecords } from './EditPerson.js'
 import gql from 'graphql-tag'
 
 export const PersonSkillsTable = ({ data, editablePerson = null, onRowClick }) => {
-  if (data.error) {
-    return (<div className="ui warning icon message">
-      <i className="warning sign icon"></i>
-      <div className="content">
-        <div className="header"> Server Error </div>
-        <p>{String('data.error')}</p>
-      </div>
-    </div>)
-  }
-  if (data.loading) {
-    return (<div className="ui active inverted dimmer">
-      <div className="ui text loader">Loading</div>
-    </div>)
-  }
-  let people = data.allUsers.edges.map(({ node }) => node);
-  let skills = data.allSkills.edges.map(({ node }) => node);
-  people.sort(({ firstName: a }, { firstName: b }) => a < b ? -1 : a > b ? 1 : 0)
-  return (<table className="ui striped selectable definition table">
-    <thead>
-      <tr className="sticky">
-        <th />
-        {skills.map(({ name, id }) => <th key={id}>{name}</th>)}
-      </tr>
-    </thead>
-    <tbody>
-      {people.map(person =>
-        <PersonSkillRow key={person.id}
-          person={person}
-          skills={skills}
-          editable={person === editablePerson}
-          onClick={() => onRowClick(person)}
-        />)}
-    </tbody>
-  </table>)
+    if (data.error) {
+        return (<div className="ui warning icon message">
+            <i className="warning sign icon"></i>
+            <div className="content">
+                <div className="header"> Server Error </div>
+                <p>{String('data.error')}</p>
+            </div>
+        </div>)
+    }
+    if (data.loading) {
+        return (<div className="ui active inverted dimmer">
+            <div className="ui text loader">Loading</div>
+        </div>)
+    }
+    let people = data.allUsers.edges.map(({ node }) => node);
+    let skills = data.allSkills.edges.map(({ node }) => node);
+    people.sort(({ firstName: a }, { firstName: b }) => a < b ? -1 : a > b ? 1 : 0)
+    return (<table className="ui striped selectable definition table">
+        <thead>
+            <tr className="sticky">
+                <th />
+                {skills.map(({ name, id }) => <th key={id}>{name}</th>)}
+            </tr>
+        </thead>
+        <tbody>
+            {people.map(person =>
+                <PersonSkillRow key={person.id}
+                    person={person}
+                    skills={skills}
+                    editable={person === editablePerson}
+                    onClick={() => onRowClick(person)}
+                />)}
+        </tbody>
+    </table>)
 }
 export const PersonSkillRow = ({ person, skills, onClick, editable }) => {
-  const personSkillRecords = getPersonSkillRecords(person, skills);
-  return (<tr onClick={onClick}>
-    <th>
-      <div>{person.firstName}</div>
-      <div className="right aligned">{person.lastName}</div>
-      {editable && <div className="ui pointing below label">
-        Edit your stars in the table below.
+    const personSkillRecords = getPersonSkillRecords(person, skills);
+    return (<tr onClick={onClick}>
+        <th>
+            <div>{person.firstName}</div>
+            <div className="right aligned">{person.lastName}</div>
+            {editable && <div className="ui pointing below label">
+                Edit your stars in the table below.
       </div>}
-    </th>
-    {personSkillRecords.map(({ skill, personSkill }) =>
-      <td key={skill.id} className="center aligned">
-        <Rating rating={personSkill.experience} icon="star" />
-        <Rating rating={personSkill.desire} icon="student" />
-      </td>
-    )}
-  </tr >)
+        </th>
+        {personSkillRecords.map(({ skill, personSkill }) =>
+            <td key={skill.id} className="center aligned">
+                <Rating rating={personSkill.experience} icon="star" />
+                <Rating rating={personSkill.desire} icon="student" />
+            </td>
+        )}
+    </tr >)
 };
 
 export const personSkillsQuery = gql`
 query {
-          allUsers {
+    allUsers {
         edges {
           node {
         id
@@ -94,7 +94,7 @@ query {
 
 
 export const PersonSkillsTableContainer = graphql(personSkillsQuery, {
-  options: {
-    errorPolicy: 'all'
-  }
+    options: {
+        errorPolicy: 'all'
+    }
 })(PersonSkillsTable);
